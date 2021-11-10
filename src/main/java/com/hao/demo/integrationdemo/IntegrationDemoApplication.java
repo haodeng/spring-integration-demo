3b10411228fafded3c4aa0ba0a49f4223b1079e4
haodeng <hao.deng.m@gmail.com>
2021-11-10 21:51:55 +0100

    first commit

---
+++ src/main/java/com/hao/demo/integrationdemo/IntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hao.demo.integrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hao/demo/integrationdemo/IntegrationFlowConfig.java
@@ -0,0 +1,35 @@
+package com.hao.demo.integrationdemo;
+
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+
+import java.util.function.Supplier;
+
+@Configuration
+@EnableIntegration
+public class IntegrationFlowConfig {
+    @Bean
+    Supplier<String> fakeAddress() {
+        return () -> new Faker().address().fullAddress();
+    }
+
+    @Bean
+    public IntegrationFlow startFlow() {
+        return IntegrationFlows.fromSupplier(fakeAddress(), c -> c.poller(Pollers.fixedRate(1000)))
+                .channel("test_channel")
+                .get();
+    }
+
+    @Bean
+    public IntegrationFlow upperCase() {
+        return IntegrationFlows.from("test_channel")
+                .transform((String s) -> s.toUpperCase())
+                .handle(x -> System.out.println(x.getPayload()))
+                .get();
+    }
+}
